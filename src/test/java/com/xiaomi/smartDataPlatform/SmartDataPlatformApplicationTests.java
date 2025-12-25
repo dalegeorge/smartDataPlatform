c28e309c71d59cb93a1b953a5080b5bae4151a38
dalegeorge <jourtom3o2@gmail.com>
2025-12-25 13:22:24 +0800

    Initial commit on project framework

---
+++ src/test/java/com/xiaomi/smartDataPlatform/SmartDataPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaomi.smartDataPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDataPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
